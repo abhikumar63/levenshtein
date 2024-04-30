b7652a74cc983f5ae1cc97d0549c78f0338aa678
Abhishek Kumar <63abhikumar@gmail.com>
2024-04-30 11:43:29 +0530

    Added main file

--- levenshtein_distance.h
+++ levenshtein_distance.h
@@ -1,14 +1,16 @@
 #include<iostream>
-#include<string.h>
+#include<string>
 #include<math.h>
 #include<algorithm>
 
-int calculateLevenshteinDistance(std::string word1, std::string word2){
+int calculateLevenshteinDistance(std::string word1, std::string word2)
+{
     size_t size1 = word1.size();
     size_t size2 = word2.size();
 
     int **lev = new int*[size1+1];
-    for(size_t i=0;i<size1+1;i++){
+    for(size_t i=0;i<size1+1;i++)
+    {
         lev[i] = new int[size2+1];
     }
 
@@ -17,15 +19,19 @@ int calculateLevenshteinDistance(std::string word1, std::string word2){
     if(size2 == 0)
         return size1;
 
-    for(size_t i = 0; i <= size1; i++){
+    for(size_t i = 0; i <= size1; i++)
+    {
         lev[i][0] = i;
     }
-    for(size_t j = 0; j <= size2; j++){
+    for(size_t j = 0; j <= size2; j++)
+    {
         lev[0][j] = j;
     }
 
-    for(size_t i = 1; i <= size1; i++){
-        for(size_t j = 1; j <= size2; j++){
+    for(size_t i = 1; i <= size1; i++)
+    {
+        for(size_t j = 1; j <= size2; j++)
+        {
             int cost = (word1[i-1] == word2[j-1]) ? 0 : 1;
 
             lev[i][j] = std::min(std::min(lev[i - 1][j] + 1, lev[i][j - 1] + 1), lev[i - 1][j - 1] + cost);
@@ -33,7 +39,8 @@ int calculateLevenshteinDistance(std::string word1, std::string word2){
     }
 
     int levVal = lev[size1][size2];
-    for(size_t i=0;i<size1+1;i++){
+    for(size_t i=0;i<size1+1;i++)
+    {
         delete[] lev[i];
     }
 
---
+++ main.cpp
@@ -0,0 +1,119 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include"levenshtein_distance.h"
+
+int main()
+{
+    std::vector<std::string> dictionary
+    {
+        "apple",
+        "apparently",
+        "appears",
+        "appreciate"
+    };
+    int choice;
+    int willContinue;
+    do
+    {
+    std::cout << "Choose a index from the list.\n";
+    std::cout << "1. Compare 2 strings using user input.\n";
+    std::cout << "2. Compare a string with in-built dictionary using default tolerance.\n";
+    std::cout << "3. Compare a string with in-built dictionary using custom tolerance.\n";
+    std::cout << "4. Exit\n";
+    std::cout << "Enter a choice: ";
+
+    std::cin >> choice;
+
+    switch (choice)
+    {
+    case 1:
+    {
+        std::string userInput1;
+        std::string userInput2;
+        std::cout << "Enter the first string: ";
+        std::cin >> userInput1;
+        std::cout << "Enter the second string: ";
+        std::cin >> userInput2;
+        std::cout << "Levenshtein distannce between " << userInput1 << " and " << userInput2 << " is " << calculateLevenshteinDistance(userInput1, userInput2) << ".";
+        break;
+    }
+    case 2:
+    {
+        std::string userInput;
+        std::cout << "Enter a string: ";
+        std::cin >> userInput;
+        std::vector<std::string> suggestions;
+        int tolerance = 2;
+
+        for(auto word : dictionary)
+        {
+            int dist = calculateLevenshteinDistance(word, userInput);
+
+            if(dist <= tolerance)
+            {
+                suggestions.push_back(word);
+            }
+        }
+
+        std::cout << "User Input :" << userInput;
+        if(suggestions.size() > 0)
+        {
+            std::cout << "Did you mean? ";
+            for(auto word : suggestions)
+            {
+                std::cout << word << " ";
+            }
+        }
+        break;
+    }
+    case 3:
+    {
+        std::string userInput;
+        std::cout << "Enter a string: ";
+        std::cin >> userInput;
+        std::vector<std::string> suggestions;
+        int tolerance;
+        std::cout << "Enter the tolerance level: ";
+        std::cin >> tolerance;
+
+        for(auto word : dictionary)
+        {
+            int dist = calculateLevenshteinDistance(word, userInput);
+
+            if(dist <= tolerance)
+            {
+                suggestions.push_back(word);
+            }
+        }
+
+        std::cout << "User Input :" << userInput;
+        if(suggestions.size() > 0)
+        {
+            std::cout << "Did you mean? ";
+            for(auto word : suggestions)
+            {
+                std::cout << word << " ";
+            }
+        }
+        break;
+    }
+    case 4:
+        break;
+    default:
+        std::cout << "Bad Choice! Try Again.";
+        break;
+    }
+    if(choice == 4)
+        break;
+    
+    std::cout << "\n\n";
+    std::cout << "Do you wish to continue? \n1. Yes \n2. No \n";
+    std::cin >> willContinue;
+    std::cout << "\n";
+    } while(willContinue == 1 && choice != 4);
+
+    std::cout << "Thank you for playting.\n";
+
+    return 0;
+}
